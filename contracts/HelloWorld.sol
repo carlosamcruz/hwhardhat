999352049290a01c165c1e6e89b4412e8e40862f
carlosamcruz <carlosamcruz@ufam.edu.br>
2024-11-17 22:38:44 -0300

    first commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract HelloWorld {
+
+    string public message = "Hello World";
+    
+    function setMessage(string memory newMessage)  public {
+        message = newMessage;
+    }
+
+
+}
